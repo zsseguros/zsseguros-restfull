401739ada873494a3a4792283495a022df208730
zscaiosi <zscaio.si@gmail.com>
2018-05-13 16:42:13 -0300

    [Sprint 5] Ajustes finos de navegação;

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,9 +1,9 @@
 module.exports = {
   "dbConfig": {
-  "host": "localhost",
-  "port": 3306,
-  "user": "root",
-  "password": "dbn*2018",
-  "db": "db_zsseguros_dev"
+    "host": "localhost",
+    "port": 3306,
+    "user": "zsapp",
+    "password": "zsapp*2018",
+    "db": "db_zsseguros_prod"
   }
 }
--- src/config/server_config.ts
+++ src/config/server_config.ts
@@ -4,11 +4,21 @@ import clientes from '../routes/clientes';
 import apolices from '../routes/apolices';
 import veiculos from '../routes/veiculos';
 import * as cors from 'cors';
+const fs = require('fs');
 
 module.exports = function(){
   const app = express();
   app.use(bodyParser.json());
   app.use(cors());
+
+  app.use('*', (req: any, res: any, next: Function) => {
+
+    fs.appendFile('/home/caio/api_logs.txt', "\n"+String(req.ip)+" - "+req.get("referer"), (error, success) => {
+      console.log(req)
+      next();
+    });
+
+  });
 
   app.use('/clientes', clientes);
   app.use('/apolices', apolices);
