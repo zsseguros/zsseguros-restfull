350ff11062600e5ad95f1fd798bcb3717c5ccec3
zscaiosi <zscaio.si@gmail.com>
2018-05-13 16:42:13 -0300

    [Sprint 5] Ajustes finos de navegação;

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -2,8 +2,8 @@ module.exports = {
   "dbConfig": {
   "host": "localhost",
   "port": 3306,
-  "user": "root",
-  "password": "dbn*2018",
-  "db": "db_zsseguros_dev"
+  "user": "zsapp",
+  "password": "zsapp*2018",
+  "db": "db_zsseguros_prod"
   }
 }
